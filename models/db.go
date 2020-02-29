7f8828f280c68974bd12e6b3e5c1cc32cef23c3d
aleale2121 <alefewyimer4@gmail.com>
2020-02-29 07:25:45 +0300

    hj

--- models/db.go
+++ models/db.go
@@ -4,21 +4,22 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	_ "os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	USER   = "postgres"
-	PASS   = "root"
-	DBNAME = "dt"
-)
+//const (
+//	USER   = "postgres"
+//	PASS   = "root"
+//	DBNAME = "dt"
+//)
 
 func Connect() *sql.DB {
-	URL := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", USER, PASS, DBNAME)
-	db, err := sql.Open("postgres", URL)
-	//db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	//URL := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", USER, PASS, DBNAME)
+	//db, err := sql.Open("postgres", URL)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
